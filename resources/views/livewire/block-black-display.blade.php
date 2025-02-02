ca076e7663939172b3371c446938ebfa4407c5cb
Vincent-Tshipamba <tshipambalubobo80@gmail.com>
2025-02-02 16:05:35 +0100

    Mise a jour de contenu pour le dernier bloc de la homepage

---
+++ resources/views/livewire/block-black-display.blade.php
@@ -0,0 +1,19 @@
+<div class="block-black">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-xxl-6">
+                <div class="text-center">
+                    <h2 class="text-xxl text-white mb-lg-4 mb-3">
+                        {!! $blockBlack->title !!}
+                    </h2>
+                    <p class="paragraph text-white mx-auto mb-lg-4 mb-3">
+                        {{ $blockBlack->description }}
+                    </p>
+                    <a href="/boutique" class="btn btn-white btn-default">
+                        Explorez et commandez
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
