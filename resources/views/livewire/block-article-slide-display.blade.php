749728e283659120dbb1ba35fe942e80f61086f7
Vincent Tshipamba <tshipambalubobo80@gmail.com>
2025-02-04 14:21:12 +0100

    Evaluation de produits (display)

--- resources/views/livewire/block-article-slide-display.blade.php
+++ resources/views/livewire/block-article-slide-display.blade.php
@@ -22,7 +22,8 @@
                                     <div class="bundel">
                                         Meilleure vente
                                     </div>
-                                    <a href="" onclick="addToWishList(event, {{ $produit->id }})" class="like tooltip-hover" data-position-tooltip="right">
+                                    <a href="" onclick="addToWishList(event, {{ $produit->id }})"
+                                        class="like tooltip-hover" data-position-tooltip="right">
                                         <svg viewBox="0 0 24 24" width="512" height="512">
                                             <path
                                                 d="M17.5,1.917a6.4,6.4,0,0,0-5.5,3.3,6.4,6.4,0,0,0-5.5-3.3A6.8,6.8,0,0,0,0,8.967c0,4.547,4.786,9.513,8.8,12.88a4.974,4.974,0,0,0,6.4,0C19.214,18.48,24,13.514,24,8.967A6.8,6.8,0,0,0,17.5,1.917Zm-3.585,18.4a2.973,2.973,0,0,1-3.83,0C4.947,16.006,2,11.87,2,8.967a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,8.967a1,1,0,0,0,2,0,4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,8.967C22,11.87,19.053,16.006,13.915,20.313Z" />
@@ -39,12 +40,14 @@
                                         <a href="{{ route('produit.show', $produit->id) }}">
                                             <h3 class="mb-lg-2">{{ $produit->nom }}</h3>
                                         </a>
+                                        @php
+                                            $averageRating = $produit->averageRating();
+                                        @endphp
                                         <div class="d-flex align-items-center gap-2 block-rate mb-lg-2 mb-1">
-                                            <i class="bi bi-star-fill active"></i>
-                                            <i class="bi bi-star-fill active"></i>
-                                            <i class="bi bi-star-fill active"></i>
-                                            <i class="bi bi-star-fill"></i>
-                                            <i class="bi bi-star-fill"></i>
+                                            @for ($i = 1; $i <= 5; $i++)
+                                                <i
+                                                    class="bi bi-star-fill {{ $i <= $averageRating ? 'active' : '' }}"></i>
+                                            @endfor
                                         </div>
                                         <div class="d-flex align-items-center">
                                             <div class="price">
@@ -81,11 +84,10 @@
                                                 Ajouter au panier
                                             </a>
                                             <div class="d-flex block-rate align-items-center gap-2 ms-auto">
-                                                <i class="bi bi-star-fill active"></i>
-                                                <i class="bi bi-star-fill active"></i>
-                                                <i class="bi bi-star-fill active"></i>
-                                                <i class="bi bi-star-fill"></i>
-                                                <i class="bi bi-star-fill"></i>
+                                                @for ($i = 1; $i <= 5; $i++)
+                                                    <i
+                                                        class="bi bi-star-fill {{ $i <= $averageRating ? 'active' : '' }}"></i>
+                                                @endfor
                                             </div>
                                         </div>
                                     </div>
